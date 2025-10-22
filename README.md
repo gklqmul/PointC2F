19fb3d055ed19996164fd44b3dc1658bc2a06bba
Kailu Guo <acw801@qmul.ac.uk>
2025-10-22 22:27:58 +0100

    init

--- README.md
+++ README.md
@@ -4,12 +4,33 @@ This is the **official implementation** of PointC2F.
 
 ## Dataset
 
-Please download the dataset here: [Dataset Download Link](xxx)
+Please download the dataset here: [Dataset Download Link](https://drive.google.com/file/d/10a3TRjzZMpEJ9Zzm7DD0vfVnZqM6jIBG/view?usp=drive_link)
+You can download that and create a new folder, named `dataset/` and put dataset here.
+
+## Environment Setup
+
+You can set up the required environment using a `requirements.txt`.
+
+1.  **Create a virtual environment:**
+    ```bash
+    python -m venv venv
+    ```
+
+2.  **Install the dependencies:**
+    ```bash
+    pip install -r requirements.txt
+    ```
 
 ## Usage
 
-The repository provides scripts for training and testing. You can run them directly:
+Once the environment is activated, you can run the provided scripts for training and testing.
 
 ### Training
 ```bash
 python train.py
+```
+
+### Testing
+```bash
+python test.py
+```
--- kfoldtrain.py
+++ kfoldtrain.py
@@ -47,38 +47,27 @@ SCALER_PATH = "scaler.gz"
 all_sample_ids = sorted([os.path.splitext(os.path.basename(f))[0].rsplit('-', 1)[0] 
                         for f in glob(os.path.join(ROOT_DIR, '*-motion.csv'))])
 
-# files = glob(os.path.join(ROOT_DIR, '*-motion.csv'))
-# all_sample_ids = sorted({
-#         sid
-#         for f in files
-#         for sid in [os.path.splitext(os.path.basename(f))[0].rsplit('-', 1)[0]]
-#         if len(sid.split('-')) >= 2 and sid.split('-')[1].isdigit() and int(sid.split('-')[1]) < 4
-# })
-
 kf = KFold(n_splits=5, shuffle=True, random_state=42)
 all_fold_results = []
 
 fold_metrics = []
 with open(os.path.join(ROOT_DIR, 'newlabel.json'), 'r') as f:
         label_dict = json.load(f)
-# if not os.path.exists(SCALER_PATH):     
-#         generate_and_save_scaler(ROOT_DIR, train_ids, SCALER_PATH)
 
 scaler = joblib.load(SCALER_PATH)
 INPUT_DIM_1D = 36
 POINT_FEATURE_DIM = 256
 NUM_CLASSES = 6 
 all_dfs = []
+# K-Fold Cross-Validation
 for fold, (train_idx, test_idx) in enumerate(kf.split(all_sample_ids)):
     print(f"Fold {fold+1}")
     
     train_ids = [all_sample_ids[i] for i in train_idx]
     test_ids  = [all_sample_ids[i] for i in test_idx]
     
-    # 可以再在 train_ids 中划分 val_ids（比如 10%）
     train_ids, val_ids = train_test_split(train_ids, test_size=0.1, random_state=42)
     
-    # 创建 Dataset 和 DataLoader
     train_dataset = TUGFeatureDataset(ROOT_DIR, train_ids, label_dict, scaler, augment=True)
     val_dataset   = TUGFeatureDataset(ROOT_DIR, val_ids,   label_dict, scaler, augment=False)
     test_dataset  = TUGFeatureDataset(ROOT_DIR, test_ids,  label_dict, scaler, augment=False)
@@ -92,19 +81,19 @@ for fold, (train_idx, test_idx) in enumerate(kf.split(all_sample_ids)):
 
     model_weights_path = "./softedgecleanpoints5dseprate.pth"
     if os.path.exists(model_weights_path):
-            print(f"正在从 {model_weights_path} 加载已适配的PointNet++权重...")
+            print(f"loading {model_weights_path} ...")
             try:
                 state_dict = torch.load(model_weights_path, map_location='cpu')
                 point_cloud_extractor.load_state_dict(state_dict)
                 point_cloud_extractor = point_cloud_extractor.to(device)
-                print("✅ 权重加载成功！")
+                print("success")
             except Exception as e:
-                print(f"❌ 加载权重失败: {e}")
+                print(f"failed: {e}")
     else:
-            print(f"⚠️ 未找到 PointNet 权重文件，使用随机初始化")
+            print(f"can't find {model_weights_path}, using random initialization")
     for param in point_cloud_extractor.parameters():
             param.requires_grad = False
-    print("已冻结 PointNet 权重")
+    print("Point cloud extractor loaded and frozen.")
     model = MultiClassGRU(NUM_CLASSES, INPUT_DIM_1D, point_cloud_extractor).to(device)
     # model = MultiClassBiLSTMNEW(
     #     num_classes=NUM_CLASSES,
@@ -159,6 +148,7 @@ for fold, (train_idx, test_idx) in enumerate(kf.split(all_sample_ids)):
     optimizer = optim.AdamW(model.parameters(), lr=1e-4, weight_decay=1e-5)
     criterion = WeightedFocalLoss(gamma=2.0, weight=class_weights).to(device)
     
+    # Training
     # best_val_f1 = 0
     # epochs = 64
     # for epoch in range(epochs):
@@ -172,6 +162,8 @@ for fold, (train_idx, test_idx) in enumerate(kf.split(all_sample_ids)):
     #         best_model_state = model.state_dict()
     #     model_path = f"./dark/fold{fold+1}_model.pth"
     #     torch.save(best_model_state, model_path)
+
+    # Testing
     model_path = f"./GRU/fold{fold+1}_model.pth"
     model.load_state_dict(torch.load(model_path, map_location='cpu'))
     test_metrics = evaluate(model, test_loader, criterion, device)
@@ -187,20 +179,20 @@ for fold, (train_idx, test_idx) in enumerate(kf.split(all_sample_ids)):
         correction_fn=sliding_window_voting
     )
     
-    # 分段指标
+
     segmentation_metrics = evaluate_comprehensive_metrics(
         all_true_labels=final_results['true_labels'],
         all_pred_labels=final_results['processed_predictions'],
         num_classes=5
     )
     
-    # 速度指标
+    
     speed = compute_speeds_with_bias_correction(final_results['true_labels'], 
                            final_results.get('processed_predictions', None), 
                            test_ids)
     speed_metrics = compute_speed_metrics(speed)
     all_dfs.append(speed)
-    # 汇总每折结果
+
     fold_result = {
         "fold": fold+1,
         **{f"raw_{k}": v for k,v in final_results['raw_metrics'].items()},
@@ -217,7 +209,7 @@ plot_gait_speeds_violin(combined_df)
 
 df_results = pd.DataFrame(all_fold_results)
 df_results.to_excel("./GRU/light.xlsx", index=False)
-print("✅ 所有折测试结果已保存到 light.xlsx")
+
 
 avg_metrics = np.mean(fold_metrics, axis=0)
 std_metrics = np.std(fold_metrics, axis=0)
